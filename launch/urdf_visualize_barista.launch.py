040e0fd9b12aff0d85cd87fc9d2fc7587e8de92f
Romu10 <romulobryanp@gmail.com>
2023-10-07 07:31:14 +0000

    URDF - Physics working and Launching Rviz/Gazebo

--- launch/urdf_visualize_barista.launch.py
+++ launch/urdf_visualize_barista.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
     )
 
     # RVIZ Configuration
-    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'barista_config.rviz')
 
 
     rviz_node = Node(
